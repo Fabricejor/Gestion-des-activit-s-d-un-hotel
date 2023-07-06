c85a0fad8c0ad7855b7c88f4d874fa55a2e4ae40
paulitoo320 <128219233+paulitoo320@users.noreply.github.com>
2023-07-06 10:25:46 +0000

    Delete Back-end Ramos/Hotel-Gestion directory

--- Back-end Ramos/Hotel-Gestion/Private/bienvenue.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-    print "<h1> bienvenue ".$_SESSION['email'] ."</h1> <br>";
-
-?>
--- Back-end Ramos/Hotel-Gestion/Public/client/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root", "root", "gestionHotel")
-or die("Connexion impossible");
-?>
